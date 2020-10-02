3d2bf8f04161c6d4eb1678b7ecdc8c7fa44f008e
Takahana <tagahana508@gmail.com>
2020-10-02 12:18:06 +0900

    fix SkyWayEvent

--- app/src/main/java/tech/takahana/skywayclient/SkyWayEvent.kt
+++ app/src/main/java/tech/takahana/skywayclient/SkyWayEvent.kt
@@ -4,25 +4,41 @@ import io.skyway.Peer.Peer
 import io.skyway.Peer.Room
 
 
-class SkyWayEvent {
-    enum class PeerEvent {
-        OPEN, CONNECTION, CALL, CLOSE, DISCONNECT, ERROR, NONE
+sealed class SkyWayEvent {
+
+    sealed class PeerEvent : SkyWayEvent() {
+
+        object OPEN : PeerEvent()
+        object CONNECTION : PeerEvent()
+        object CALL : PeerEvent()
+        object CLOSE : PeerEvent()
+        object DISCONNECT : PeerEvent()
+        object ERROR : PeerEvent()
+        object UNKNOWN : PeerEvent()
     }
-    enum class RoomEvent {
-        STREAM, REMOVE_STREAM, OPEN, CLOSE, PEER_JOIN, PEER_LEAVE, ERROR, NONE
+
+    sealed class RoomEvent : SkyWayEvent() {
+        object STREAM : RoomEvent()
+        object REMOVE_STREAM : RoomEvent()
+        object OPEN : RoomEvent()
+        object CLOSE : RoomEvent()
+        object PEER_JOIN : RoomEvent()
+        object PEER_LEAVE : RoomEvent()
+        object ERROR : RoomEvent()
+        object UNKNOWN : RoomEvent()
     }
 
 
     companion object {
         fun from(event: Peer.PeerEventEnum): PeerEvent {
-            return when(event) {
+            return when (event) {
                 Peer.PeerEventEnum.OPEN -> PeerEvent.OPEN
                 Peer.PeerEventEnum.CONNECTION -> PeerEvent.CONNECTION
                 Peer.PeerEventEnum.CALL -> PeerEvent.CALL
                 Peer.PeerEventEnum.CLOSE -> PeerEvent.CLOSE
                 Peer.PeerEventEnum.DISCONNECTED -> PeerEvent.DISCONNECT
                 Peer.PeerEventEnum.ERROR -> PeerEvent.ERROR
-                else -> PeerEvent.NONE
+                else -> PeerEvent.UNKNOWN
             }
         }
         fun from(event: Room.RoomEventEnum): RoomEvent {
@@ -34,7 +50,7 @@ class SkyWayEvent {
                 Room.RoomEventEnum.PEER_JOIN -> RoomEvent.PEER_JOIN
                 Room.RoomEventEnum.PEER_LEAVE -> RoomEvent.PEER_LEAVE
                 Room.RoomEventEnum.ERROR -> RoomEvent.ERROR
-                else -> RoomEvent.NONE
+                else -> RoomEvent.UNKNOWN
             }
         }
     }
