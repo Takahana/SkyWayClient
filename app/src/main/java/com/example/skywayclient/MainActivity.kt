1c3b60d82a49249ffc02ea8f598b20af76a5a67b
Takahana <tagahana508@gmail.com>
2020-10-03 18:54:02 +0900

    fix

--- app/src/main/java/com/example/skywayclient/MainActivity.kt
+++ app/src/main/java/com/example/skywayclient/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        skyWayClient = SkyWayClient(this).initialize()
+        if (!::skyWayClient.isInitialized) skyWayClient = SkyWayClient(this).initialize()
 
         start_btn.setOnClickListener {
             startConnection()
