79d75a792715fea08cd39fb18f76432513f2ef96
Takahana <tagahana508@gmail.com>
2021-03-05 15:34:45 +0900

    Allow PeerOption to be set.

--- app/src/main/java/com/example/skywayclient/MainActivity.kt
+++ app/src/main/java/com/example/skywayclient/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import io.skyway.Peer.Peer
+import io.skyway.Peer.PeerOption
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
@@ -12,23 +14,32 @@ import tech.takahana.skywayclient.SkyWayEvent
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var skyWayClient: SkyWayClient
+    private val skyWayClient = SkyWayClient()
+    private val PEER_ID = "peer1234"
     private val ROOM_ID = "room1234"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (!::skyWayClient.isInitialized) skyWayClient =
-            SkyWayClient(this).initialize()
 
         start_btn.setOnClickListener {
             startConnection()
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        skyWayClient.requestPermission(this)
+    }
+
     private fun startConnection() {
+        val option = PeerOption().apply {
+            key = getString(tech.takahana.skywayclient.R.string.skyway_api_key)
+            domain = getString(tech.takahana.skywayclient.R.string.skyway_domain)
+            debug = Peer.DebugLevelEnum.ALL_LOGS
+        }
         lifecycleScope.launchWhenResumed {
-            skyWayClient.connect()
+            skyWayClient.connect(this@MainActivity, PEER_ID, option)
                 .catch { error ->
                     // handle exception: ex) permission denied
                     Log.e("_ERROR_", "${error.message}")
